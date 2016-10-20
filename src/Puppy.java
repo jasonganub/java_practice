1943a1a8712e35e1efa10461ccb796fd55f50c1b
Jason Ganub <jasonganub@gmail.com>
2016-10-19 23:59:53 -0700

    Added getter and setters with an instance variable

--- src/Puppy.java
+++ src/Puppy.java
@@ -2,13 +2,33 @@
  * Created by jasonganub on 10/19/16.
  */
 public class Puppy {
+    int puppyAge;
+
     public Puppy(String name) {
         // Constructor with one parameter, name
         System.out.println("Passed Name is: " + name);
     }
 
+    public void setAge(int age) {
+        puppyAge = age;
+    }
+
+    public int getAge() {
+        System.out.println("Puppy's age is : " + puppyAge);
+        return puppyAge;
+    }
+
     public static void main(String []args) {
         // Creating the myPuppy object
         Puppy myPuppy = new Puppy("Jason");
+
+        // Call class method to set puppy's age
+        myPuppy.setAge(24);
+
+        // Call another class method to get puppy's age
+        myPuppy.getAge();
+
+        // You can access instance variable as follows as well
+        System.out.println("Variable value : " + myPuppy.puppyAge);
     }
 }
